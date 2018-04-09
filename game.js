e6755e40b7c688d1608ea66a29d512f329ec2f2a
Hindia <mahmmudhindia@gmail.com>
2018-04-09 17:09:32 +0300

    update 2

--- game.js
+++ game.js
@@ -1,13 +1,18 @@
 //settings
-var playerX=2;
-var playerY=2;
-var height=30;
-var width=45;
+var snakeX=2;
+var snakeY=2;
+var height=20;
+var width=24;
 var speed=100;
 var increment=2;
 
 //game variables
+var length =0;
 var score =0;
+var tailX=[snakeX];
+var tailY=[snakeY];
+var fX;
+var fY;
 var running=false;
 var gameOver=false;
 var direction=1;//right 1, left -1, down -2, up 2
@@ -22,28 +27,29 @@ function run(){
 //initializing the game
 function init(){
 	createMap();
-	createPlayer();
+	createSnake();
+	createFruit();
 }
 
-//generates the map
+//generates the map for the snake
 function createMap(){
 	document.write("<table>");
 	for(var y = 0;y < height;y++){
 		document.write("<tr>");
 		for(var x = 0;x < width;x++){
 			if(x == 0 || x == width-1 || y == 0 || y == height-1)
-				document.write("<td class='wall' id='"+x+ "-" +y+"'></td>");	//marks the wall with black table cells
+				document.write("<td class='wall' id='"+x+ "-" +y+"'></td>");	//marks the wall with red table cells
 			else
-				document.write("<td class='blank' id='"+x+ "-" +y+"'></td>");	//marks the inside with gray(color for blank set in css file) table cells
+				document.write("<td class='blank' id='"+x+ "-" +y+"'></td>");	//marks the inside with black(color for blank set in css file) table cells
 		}
 		document.write("</tr>");
 	}
 	document.write("</table>");
 }
 
-//generates a player
-function createPlayer(){
-	setType(playerX,playerY,"player");
+//generates a snake
+function createSnake(){
+	setType(snakeX,snakeY,"snake");
 }
 
 //gets the x y coordinate values
@@ -57,6 +63,18 @@ function setType(x,y,value){
 		get(x,y).setAttribute("class",value);
 }
 
+//generates the fruits that pop randomly
+function createFruit(){
+	var found = false;
+	while(!found && (length < (width-2) * (height-2) + 1)){
+		var fruitX = rand(1, width-1);
+		var fruitY = rand(1,height-1);
+		if(getType(fruitX,fruitY) == "blank") found=true;
+	}
+	setType(fruitX,fruitY,"fruit");
+	fX=fruitX;
+	fY=fruitY;
+}
 
 //creates a random value between the min and max
 function rand(min,max){
@@ -93,21 +111,39 @@ function gameLoop(){
 
 //constantly updates the position of the snake
 function update(){
-	if(direction==2) playerY--;
-	else if (direction==-2) playerY++;
-	else if (direction==1) playerX++;
-	else if (direction==-1) playerX--;
-	setType(playerX,playerY,"player");
-
-	if (playerX == 0 || playerX == width-1 || playerY == 0 || playerY == height-1)	//if it hits the wall
+	setType(fX, fY, "fruit");
+	updateTail();
+	setType(tailX[length], tailY[length], "blank");
+	if(direction==2) snakeY--;
+	else if (direction==-2) snakeY++;
+	else if (direction==1) snakeX++;
+	else if (direction==-1) snakeX--;
+	setType(snakeX,snakeY,"snake");
+	for(var i=tailX.length-1; i>= 0; i--){
+		if(snakeX == tailX[i] && snakeY == tailY[i]){	//if it hits/eats itself
+			gameOver=true;
+			break;
+		}
+	}
+	if (snakeX == 0 || snakeX == width-1 || snakeY == 0 || snakeY == height-1)	//if it hits the wall
 		gameOver=true;
-	else {
+	else if(snakeX == fX && snakeY==fY){
 		score+=2;
+		createFruit();
+		length+=increment;
 
 	}
 	document.getElementById("score").innerHTML = "Score: "+ score;
 }
 
-
+//updates the position of the tail of the snake
+function updateTail(){
+	for(var i=length; i>0 ;i--){
+		tailX[i]=tailX[i-1];
+		tailY[i]=tailY[i-1];
+	}
+	tailX[0]=snakeX;
+	tailY[0]=snakeY;
+}
 
 run();
