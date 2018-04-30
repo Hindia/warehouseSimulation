f146879b3879326f7124e49e3188e277ea474548
Hindia <mahmmudhindia@gmail.com>
2018-04-30 18:30:35 +0300

    animate

--- game.js
+++ game.js
@@ -3,7 +3,7 @@ canvas.width=window.innerWidth;
 canvas.height=window.innerHeight;
 var c = canvas.getContext("2d");
 var img = new Image();
-img.src = "person.png";
+img.src = "img/person.png";
  c.drawImage(img, 50, canvas.height-150,100,100);
 
 c.font="30px papyrus";
@@ -25,13 +25,47 @@ c.lineTo(10,canvas.height-50);
 c.lineTo(10,100);
 c.stroke();
 
+//hints
+var hintsArray =[
+	'use arrow keys to move',
+	'click the item to pick it',
+	'doubleclick to swap',
+	'clock is ticking!!',
+	'Take the shortest route ',
+	'You are almost done',
+];
+
+var colorArray =[
+	'black',
+	'green',
+	'red',
+	'blue',
+	'purple ',
+];
+
+function animate(){
+	
+	setTimeout(function(){
+		requestAnimationFrame(animate);
+	},1300);
+	c.clearRect(canvas.width-400,0, canvas.width,canvas.height-850);
+	var message=hintsArray[Math.floor(Math.random()*hintsArray.length)];
+	var color= colorArray[Math.floor(Math.random()*colorArray.length)];
+	c.font="30px jokerman";
+	c.fillStyle=color;
+	c.fillText(message, canvas.width-400,canvas.height-900);
+	
+
+}
+animate();
+c.fillStyle='black';
 //counters
 c.font="20px Tw Cen MT";
-c.fillText("Time Lapse", canvas.width-150,canvas.height-600);
+c.fillText("Time Lapse", canvas.width-180,canvas.height-800);
 
 c.font=" 20px Tw Cen MT";
-c.fillText("Total Steps", canvas.width-150,canvas.height-500);
+c.fillText("Total Steps", canvas.width-180,canvas.height-700);
 
 //pickup list
 c.font="20px Tw Cen MT";
-c.fillText("Pick up list", canvas.width-150,canvas.height-400);
+c.fillText("Pick up list", canvas.width-180,canvas.height-600);
